5726ae5c0099914b618fb3035ce60941d9a181da
Matt Jennings <matt@mattjennings.net>
2020-08-12 14:08:22 -0700

    middle of adding title to pages

--- wp-content/themes/10up-mj-theme/header.php
+++ wp-content/themes/10up-mj-theme/header.php
@@ -7,7 +7,7 @@
 
     <meta name="viewport" content="width=device-width, initial-scale = 1.0, user-scalable=yes" />
 
-    <title><?php wp_title(''); ?></title>
+    <title><?php wp_title(); ?></title>
 
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" />
 
