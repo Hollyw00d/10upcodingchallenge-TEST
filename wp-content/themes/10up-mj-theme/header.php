319098ac93546375cbdf184deea131c67bec93b9
Matt Jennings <matt@mattjennings.net>
2020-08-12 13:59:57 -0700

    added title tag

--- wp-content/themes/10up-mj-theme/header.php
+++ wp-content/themes/10up-mj-theme/header.php
@@ -7,6 +7,8 @@
 
     <meta name="viewport" content="width=device-width, initial-scale = 1.0, user-scalable=yes" />
 
+    <title><?php wp_title(''); ?></title>
+
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" />
 
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
