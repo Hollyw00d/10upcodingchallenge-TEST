d3c70a89e451cda152350655bd9cb8a0987dfa80
Matt Jennings <matt@mattjennings.net>
2020-08-11 21:20:16 -0700

    added basic index.php, footer.php, header.php, and style.css

---
+++ wp-content/themes/10up-mj-theme/footer.php
@@ -0,0 +1,4 @@
+  <?php wp_footer(); ?>
+  <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/local.min.js"></script>
+  </body>
+</html>
---
+++ wp-content/themes/10up-mj-theme/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+
+    <meta name="viewport" content="width=device-width, initial-scale = 1.0, user-scalable=yes" />
+
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" />
+
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+
+    <?php wp_head(); ?>
+
+</head>
+
+<body <?php body_class(); ?>>
+
+<h1><?php the_title(); ?></h1>
