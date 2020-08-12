dab81434dd492d77093a407215a7500c8208ff91
Matt Jennings <matt@mattjennings.net>
2020-08-11 20:25:08 -0700

    added WP Engine gitignroe

---
+++ .wpengine-conf/wp-config-site.php
@@ -0,0 +1,5 @@
+<?php
+# GENERATED AUTOMATICALLY ON 2020-08-11 14:56:49.380624 -0700 PDT m=+59.797035275
+$table_prefix = "wp_";
+$wpengine_primary_original_domain = "tenupcodetest.wpengine.com";
+$wpengine_primary_original_ip_address = "104.198.110.24";
