93d3b6bb5f7607f0cd0c4c995d7bf8bf1fc709fb
Matt Jennings <matt@mattjennings.net>
2020-08-12 22:03:04 -0700

    done with search form for desktop

--- wp-content/themes/10up-mj-theme/inc/search-form.php
+++ wp-content/themes/10up-mj-theme/inc/search-form.php
@@ -1,5 +1,5 @@
 <form role="search" method="get" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" aria-labelledby="search-btn-<?php echo $large_rand_num_accessibility; ?>" id="search-form-<?php echo $large_rand_num_accessibility; ?>">
   <label class="screen-reader-text" for="s"><?php echo _x( 'Search website', 'label' ); ?></label>
   <input type="text" value="<?php echo esc_html(get_search_query()); ?>" placeholder="<?php echo esc_attr_x( 'e.g. delicious sandwiches', 'placeholder' ) ?>"  name="s" class="s search-form-text-input-box" />
-  <button type="submit">Submit</button>
+  <button type="submit"><img src="<?php echo get_template_directory_uri(); ?>/images/icons/search-black.svg" alt="" /></button>
 </form>
