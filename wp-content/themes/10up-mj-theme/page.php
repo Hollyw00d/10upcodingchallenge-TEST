3ff776990ab0fedd2a4e2fd9bf9e4b02cadaa8e8
Matt Jennings <matt@mattjennings.net>
2020-08-12 19:30:17 -0700

    added heading for pages

--- wp-content/themes/10up-mj-theme/page.php
+++ wp-content/themes/10up-mj-theme/page.php
@@ -2,6 +2,8 @@
 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
+    <?php echo '<h1>' . get_the_title() . '</h1>'; ?>
+
     <?php the_content(); ?>
 
 <?php endwhile; endif; ?>
