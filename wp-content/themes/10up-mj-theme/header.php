02c8b688007886427a7098b1484e7580d3d961c4
Matt Jennings <matt@mattjennings.net>
2020-08-12 18:37:37 -0700

    did desktop heading area

--- wp-content/themes/10up-mj-theme/header.php
+++ wp-content/themes/10up-mj-theme/header.php
@@ -22,36 +22,41 @@
 <body <?php body_class(); ?>>
 
 <header>
-<div id="header">
+    <div id="header">
 
-<div id="wrapper-header-inner">
+        <div class="wrapper-header-inner">
 
-    <div id="header-top-light-blue">
+            <div id="header-top-light-blue">
 
-        <div id="logo-wrapper">
-            <img id="logo" src="<?php echo get_template_directory_uri(); ?>/images/logo.svg" alt="Burger Eat Logo" />
-            <h1>Eats.</h1>
+                <div id="logo-wrapper">
+                    <a href="<?php echo get_home_url(); ?>">
+                        <img id="logo" src="<?php echo get_template_directory_uri(); ?>/images/logo.svg" alt="Burger Eat Logo" />
+                        <h1>Eats.</h1>
+                    </a>
+                </div>
+
+                <button id="search" aria-label="Search website">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/icons/search-white.svg" alt="" />
+                </button>
+            </div>
+
         </div>
 
-        <button id="search" aria-label="Search website">
-            <img src="<?php echo get_template_directory_uri(); ?>/images/icons/search-white.svg" alt="" />
-        </button>
+        <div class="med-blue-bg">
+            <div class="wrapper-header-inner">
+                <nav>
+                    <?php wp_nav_menu( array( 
+                        'sort_column' => 'menu_order', 
+                        'container_class' => 'menu-header', 
+                        'menu_class' => 'nav', 
+                        'container' => false
+                    ));
+                    ?>
+                </nav>
+            </div>
+        </div>
+
     </div>
-
-    <nav>
-    <?php wp_nav_menu( array( 
-        'sort_column' => 'menu_order', 
-        'container_class' => 'menu-header', 
-        'menu_class' => 'nav', 
-        'container' => false
-    ));
-    ?>
-    </nav>
-
-    </div>
-
-</div>
-
 </header>
 
 <div id="wrapper-content">
