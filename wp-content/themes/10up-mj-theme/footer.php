0327f12d7d83e4e52f961540d4fd2e496c290d99
Matt Jennings <matt@mattjennings.net>
2020-08-12 21:13:18 -0700

    successfully added jQuery

--- wp-content/themes/10up-mj-theme/footer.php
+++ wp-content/themes/10up-mj-theme/footer.php
@@ -4,6 +4,6 @@
   </div>
 
   <?php wp_footer(); ?>
-  <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/local.min.js"></script>
+  <!-- <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/local.min.js"></script> -->
   </body>
 </html>
--- wp-content/themes/10up-mj-theme/functions.php
+++ wp-content/themes/10up-mj-theme/functions.php
@@ -10,3 +10,9 @@ function custom_excerpt_length( $length ) {
   return 30;
 }
 add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
+
+// Add JS, including jQuery
+function local_js() {
+  wp_enqueue_script( 'local_js', get_template_directory_uri() . '/js/local.min.js', array( 'jquery' ), '1.0.0', true );
+}
+add_action( 'wp_enqueue_scripts', 'local_js' );
